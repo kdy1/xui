a79453e4594b7f59997042293ca24258a57790fc
강동윤 <kdy1997.dev@gmail.com>
2020-01-29 21:19:38 +0900

    Constraints

--- core/src/constraints.rs
+++ core/src/constraints.rs
@@ -1,1 +1,17 @@
-pub trait Constraints {}
+use std::{fmt::Debug, hash::Hash};
+
+pub trait Constraints: Debug + Clone + PartialEq + Hash {}
+
+#[derive(Debug, Clone, Copy, PartialEq, Hash)]
+pub struct BoxConstraints {}
+
+impl Constraints for BoxConstraints {}
+
+#[derive(Debug, Clone, Copy, PartialEq, Hash)]
+pub struct SliverConstraints {}
+
+impl Constraints for SliverConstraints {}
+
+impl SliverConstraints {
+    pub fn as_box_constrains(&self) -> BoxConstraints {}
+}
