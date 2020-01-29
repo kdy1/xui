0c8961a069eac7c8dbd340cfaa608f37c0f3e66c
강동윤 <kdy1997.dev@gmail.com>
2020-01-29 20:03:30 +0900

    stretch

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,1 +1,36 @@
+#[cfg(test)]
+mod tests {
+    use stretch::{geometry::Size, style::*};
 
+    #[test]
+    fn test() -> Result<(), stretch::Error> {
+        let mut stretch = stretch::node::Stretch::new();
+        let child = stretch.new_node(
+            Style {
+                size: Size {
+                    width: Dimension::Percent(0.5),
+                    height: Dimension::Auto,
+                },
+                ..Default::default()
+            },
+            vec![],
+        )?;
+
+        let node = stretch.new_node(
+            Style {
+                size: Size {
+                    width: Dimension::Points(100.0),
+                    height: Dimension::Points(100.0),
+                },
+                justify_content: JustifyContent::Center,
+                ..Default::default()
+            },
+            vec![child],
+        )?;
+
+        stretch.compute_layout(node, Size::undefined())?;
+        dbg!(stretch.layout(node)?);
+
+        Ok(())
+    }
+}
